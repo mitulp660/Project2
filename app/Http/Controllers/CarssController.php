f8508c110da8898120014a99d1c21abfcd09b629
mitul <mitulp660@gmail.com>
2019-07-26 21:20:20 -0400

    5.  Create a factory for cars that sets the make field to either ford / honda / toyota

---
+++ app/Http/Controllers/CarssController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Carss;
+use Illuminate\Http\Request;
+
+class CarssController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Carss  $carss
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Carss $carss)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Carss  $carss
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Carss $carss)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Carss  $carss
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Carss $carss)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Carss  $carss
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Carss $carss)
+    {
+        //
+    }
+}
---
+++ database/factories/CarsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Carss;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Carss::class, function (Faker $faker) {
+    return [
+        'make' => $faker->randomElement(['ford' ,'honda', 'toyota']),
+        'model' => $faker->company,
+        'year' => $faker->year,
+    ];
+});
